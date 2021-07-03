c67836851387cc74e57f8f9fd63f62c30860f070
Holms <holms200990@gmail.com>
2021-07-04 00:19:34 +0300

    Search first version. Not working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const token = 'A0MN5MW-D2WMV7F-GDWHJG6-ZBCA14Y';
+const searchButton = document.querySelector('.page-main__search-button');
+const searchField = document.querySelector('.page-main__search-field');
+const {log} = console;
+
+searchButton.addEventListener('click', async function(evt) {
+    evt.preventDefault();
+    let response = await fetch('https://api.kinopoisk.dev/movie?field=name&search=' + searchField.textContent + '&isStrict=false&token=' + token);
+    let json = response.json();
+    log(json);
+})
