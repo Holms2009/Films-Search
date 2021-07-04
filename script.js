e09c0f2421ec013aa59c3df401ce0b52e409f6ad
Holms <holms200990@gmail.com>
2021-07-04 19:36:11 +0300

    Ready to fetch

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 'use strict'
 
+const filmsButton = document.querySelector('.films-btn');
+const personsButton = document.querySelector('.persons-btn');
+const searchHeader = document.querySelector('.page-main__header');
+let searchType = 'movie';
 const token = 'A0MN5MW-D2WMV7F-GDWHJG6-ZBCA14Y';
 const searchButton = document.querySelector('.page-main__search-button');
 const searchField = document.querySelector('.page-main__search-field');
-const {log} = console;
+const { log } = console;
 
-searchButton.addEventListener('click', async function(evt) {
+filmsButton.addEventListener('click', evt => {
     evt.preventDefault();
-    let response = await fetch('https://api.kinopoisk.dev/movie?field=name&search=' + searchField.textContent + '&isStrict=false&token=' + token);
-    let json = response.json();
+    if (searchHeader.textContent !== 'Поиск фильмов') {
+        searchHeader.style = 'filter: blur(10px); color: #ffffff';
+        setTimeout(() => searchHeader.textContent = 'Поиск фильмов', 300);
+        setTimeout(() => searchHeader.style = 'filter: blur(0); color: #000000', 350);
+        searchField.placeholder = 'Введите название или часть названия фильма';
+        searchType = 'movie';
+    } else {
+        return;
+    }
+})
+
+personsButton.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (searchHeader.textContent !== 'Поиск фктеров') {
+        searchHeader.style = 'filter: blur(10px); color: #ffffff';
+        setTimeout(() => searchHeader.textContent = 'Поиск актеров', 300);
+        setTimeout(() => searchHeader.style = 'filter: blur(0); color: #000000', 350);
+        searchField.placeholder = 'Введите имя актера или режиссера';
+        searchType = 'person';
+    } else {
+        return;
+    }
+})
+
+searchButton.addEventListener('click', async function (evt) {
+    evt.preventDefault();
+    let request = 'https://api.kinopoisk.dev/' + searchType + '?field=name&search=' + searchField.value;
+    let response = await fetch(request + `&isStrict=false&token=${token}`);
+    let json = response.json().catch(log);
     log(json);
 })
