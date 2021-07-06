518198e308537957f16f1ed29f2f175ead9fa8b8
Holms2009 <holms200990@gmail.com>
2021-07-06 23:06:20 +0300

    Working search

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const token = 'A0MN5MW-D2WMV7F-GDWHJG6-ZBCA14Y';
 const filmsButton = document.querySelector('.films-btn');
 const personsButton = document.querySelector('.persons-btn');
 const searchHeader = document.querySelector('.page-main__header');
-let searchType = 'movie';
 const searchButton = document.querySelector('.page-main__search-button');
+let results = document.querySelector('.page-main__results');
+let searchType = 'movie';
 let searchField = document.querySelector('.page-main__search-field');
+let films;
 const { log } = console;
 
 filmsButton.addEventListener('click', evt => {
@@ -39,10 +41,33 @@ personsButton.addEventListener('click', evt => {
 
 searchButton.addEventListener('click', async function (evt) {
     evt.preventDefault();
+    while (results.firstChild) {
+        results.removeChild(results.lastChild);
+    }
+
     let request = 'https://api.kinopoisk.dev/' + searchType + '?field=name&search=' + searchField.value;
     let response = await fetch(request + `&isStrict=false&token=${token}`);
-    log(response);
-    // let json = response.json().catch(log);
-    // log(json);
+    response.json().then(result => {
+        films = result.docs.map(item => item);
+    }).then(() => addCards(films)).catch(log);
 })
 
+function addCards(films) {
+    log(films);
+    const cardTemplate = document.querySelector('.page-main__card-template').content.querySelector('.page-main__card');
+    films.forEach(film => {
+        let card = cardTemplate.cloneNode(true),
+            img = card.querySelector('.page-main__poster'),
+            title = card.querySelector('.page-main__title'),
+            id = card.querySelector('.page-main__kp-id'),
+            description = card.querySelector('.page-main__description'),
+            year = card.querySelector('.page-main__release-year');
+        
+        img.src = film.poster.url;
+        title.innerHTML = film.name;
+        id.innerHTML = 'ID: ' + film.id;
+        description.innerHTML = film.description;
+        year.innerHTML = 'Год выпуска: ' + film.year;
+        results.append(card);
+    });
+}
