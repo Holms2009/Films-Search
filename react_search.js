44945f7829cf137c37c5880e557fc17b7b61c753
Holms2009 <holms200990@gmail.com>
2021-07-09 23:06:43 +0300

    Added some React

--- react_search.js
+++ react_search.js
@@ -8,43 +8,68 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var LikeButton = function (_React$Component) {
-  _inherits(LikeButton, _React$Component);
+var domContainer = document.querySelector('.page-main');
 
-  function LikeButton(props) {
-    _classCallCheck(this, LikeButton);
+var App = function (_React$Component) {
+  _inherits(App, _React$Component);
 
-    var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
+  function App() {
+    _classCallCheck(this, App);
 
-    _this.state = { liked: false };
-    return _this;
+    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
   }
 
-  _createClass(LikeButton, [{
-    key: "render",
+  _createClass(App, [{
+    key: 'render',
     value: function render() {
-      var _this2 = this;
+      return React.createElement('div', { className: 'page' });
+    }
+  }]);
 
-      if (this.state.liked) {
-        return React.createElement(
-          "button",
-          { className: "like_button" },
-          "Liked"
-        );
-      }
+  return App;
+}(React.Component);
 
+var Form = function (_React$Component2) {
+  _inherits(Form, _React$Component2);
+
+  function Form() {
+    _classCallCheck(this, Form);
+
+    return _possibleConstructorReturn(this, (Form.__proto__ || Object.getPrototypeOf(Form)).apply(this, arguments));
+  }
+
+  _createClass(Form, [{
+    key: 'render',
+    value: function render() {
       return React.createElement(
-        "button",
-        { onClick: function onClick() {
-            return _this2.setState({ liked: true });
-          }, className: "like_button" },
-        "Like"
+        'form',
+        { method: this.props.method, className: this.props.class },
+        React.createElement(InputElement, { type: 'input', 'class': 'page-main__search-field', plholder: '\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u043D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u0438\u043B\u0438 \u0447\u0430\u0441\u0442\u044C \u043D\u0430\u0437\u0432\u0430\u043D\u0438\u044F \u0444\u0438\u043B\u044C\u043C\u0430' }),
+        React.createElement(InputElement, { type: 'submit', 'class': 'page-main__search-button', value: '\u041F\u043E\u0438\u0441\u043A' })
       );
     }
   }]);
 
-  return LikeButton;
+  return Form;
 }(React.Component);
 
-var domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(React.createElement(LikeButton, null), domContainer);
+var InputElement = function (_React$Component3) {
+  _inherits(InputElement, _React$Component3);
+
+  function InputElement() {
+    _classCallCheck(this, InputElement);
+
+    return _possibleConstructorReturn(this, (InputElement.__proto__ || Object.getPrototypeOf(InputElement)).apply(this, arguments));
+  }
+
+  _createClass(InputElement, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement('input', { type: this.props.type, className: this.props.class, value: this.props.value, placeholder: this.props.plholder });
+    }
+  }]);
+
+  return InputElement;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Form, { method: 'GET', 'class': 'page-main__form' }), domContainer);
